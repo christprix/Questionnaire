07e8e04fed64494508ca4e174c559a89bae82059
Christian Stprix <christprix@gmail.com>
2023-11-05 14:17:48 -0500

    created flash cards with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Flashcardlist from "./Flashcardlist";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Flashcardlist flashcards={flashcards} />
   )
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "what is 2 + 2",
+    answer: "Answer: 4",
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+
+  },
+  {
+    id: 1,
+    question: "what is 3 + 2",
+    answer: "Answer: 5",
+    options: [
+      '1',
+      '3',
+      '4',
+      '5'
+    ]
+
+  }
+]
 
 export default App
